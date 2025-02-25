4ebade1bf10b874186dacb3c327d10c74997aa00
leewonhee-3054 <dldnjsgml3054@naver.com>
2025-02-26 00:16:02 +0900

    Update UserDto.java

--- src/main/java/pratice/crud/DTO/UserDto.java
+++ src/main/java/pratice/crud/DTO/UserDto.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 // DTO 는 클라이언트와 데이터를 주고받을 때 사용하는 객체.
-// 엔티티와 직접 주고받으면 보안 문제나 유지보수 문제가 발생할 수 있어.
+// 엔티티와 직접 주고받으면 보안 문제나 유지보수 문제가 발생할 수 있음.
 
 @Getter
 @Setter
